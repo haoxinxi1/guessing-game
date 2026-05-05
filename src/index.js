944b395eaaa22cf4ac67b979c6d2f15db306e690
haoxinxi14@gmail.com <haoxinxi14@gmail.com>
2026-05-05 19:14:40 +0700

    fix: deleted call guess and replace floor with ceil

--- src/index.js
+++ src/index.js
@@ -11,18 +11,16 @@ class GuessingGame {
   }
 
   guess() {
-    this.lastGuess = Math.floor((this.low + this.high) / 2);
+    this.lastGuess = Math.ceil((this.low + this.high) / 2);
     return this.lastGuess;
   }
 
   lower() {
     this.high = this.lastGuess;
-    return this.guess();
   }
 
   greater() {
     this.low = this.lastGuess;
-    return this.guess();
   }
 }
 
